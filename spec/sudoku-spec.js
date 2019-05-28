7a32d2e324827c0819c9d44d47ebe1deae178f4e
brian hensley and megan schulte <brian@>
2019-05-28 16:31:02 -0700

    add fun new function and test

--- spec/sudoku-spec.js
+++ spec/sudoku-spec.js
@@ -1,4 +1,5 @@
 import { Sudoku } from './../src/sudoku.js';
+import { Fib } from './../src/sudoku.js';
 
 describe('Sudoku', function() {
 
@@ -6,5 +7,17 @@ describe('Sudoku', function() {
   it('should correctly determine whether any elements in array repeat themselves', function() {
     var sudoku = new Sudoku([2,3,4,5,6,7,8,9,1], [2,3,4,5,6,7,8,9,1])
     expect(sudoku.checkWin()).toEqual(true);
+  });
+});
+
+// PROJECT EULER PRACTICE
+
+describe('Fib', function() {
+
+  it('should create an array of only even fibernacci numbers', function() {
+    var evenFibs = Fib()
+    expect(evenFibs[0]).toEqual(2);
+    expect(evenFibs[1]).toEqual(8);
+    expect(evenFibs[2]).toEqual(34);
   });
 });
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,21 +1,34 @@
 export function Sudoku(row, column) {
   this.row = row;
   this.column = column;
+
+  var legal = "1,2,3,4,5,6,7,8,9";
+  // check is legal, not win
+  Sudoku.prototype.checkWin = function() {
+    var sortedRow = this.row.sort().join();
+    var sortedColumn = this.column.sort().join();
+    if (sortedRow === legal && sortedColumn === legal) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
-var legal = "1,2,3,4,5,6,7,8,9";
+// PROJECT EULER PRACTICE
+var a = 1;
+var c = 0;
 
-  // check is legal, not win
+export function Fib() {
+  var evenFibs = [2];
+  
+  for (var b = 2; b < 4000000; b = c) {
 
-Sudoku.prototype.checkWin = function() {
-  var sortedRow = this.row.sort().join();
-  var sortedColumn = this.column.sort().join();
-  console.log(sortedRow);
-  console.log(sortedColumn);
-  if (sortedRow === legal && sortedColumn === legal) {
-    return true;
-  } else {
-    return false;
+    c = a + b;
+    a = b;
+    if (c % 2 === 0) {
+      evenFibs.push(c)
+    }
   }
-
-};
+  return evenFibs;
+}
